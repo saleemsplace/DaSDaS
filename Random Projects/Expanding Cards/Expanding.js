48f0d72684bae4a1cbb5c9ef9ee1fbe3b0522e54
Your name hereSaleem <saleemsplace@aol.com>
2020-12-04 18:26:18 -0800

    Prettier

--- Random Projects/Expanding Cards/Expanding.js
+++ Random Projects/Expanding Cards/Expanding.js
@@ -1,15 +1,14 @@
-const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll(".panel");
 
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeClass();
+    panel.classList.add("active");
+  });
+});
 
-panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeClass()
-        panel.classList.add('active')
-    })
-})
-
-function removeClass(){
-    panels.forEach(panel => {
-    panel.classList.remove('active')
-    })
+function removeClass() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
 }
