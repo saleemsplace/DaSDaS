75e2e1c57ed6e97f8b6aa95dc2ba4541c115e0f1
Your name hereSaleem <saleemsplace@aol.com>
2020-12-08 15:29:27 -0800

    Wave transion js

---
+++ Random Projects/WaveForm/Waveform.js
@@ -0,0 +1,11 @@
+const label = document.querySelectorAll(".form-control Label");
+
+label.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
+    )
+    .join("");
+});
