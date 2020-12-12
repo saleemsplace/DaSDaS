bbb196a0b5892ae47a4e8c7f3cde9ed9f2d6b728
Your name hereSaleem <saleemsplace@aol.com>
2020-12-11 18:12:48 -0800

    Toggle class react

--- Learning React/First React/firstreactproject/src/Tweet.js
+++ Learning React/First React/firstreactproject/src/Tweet.js
@@ -7,7 +7,7 @@ function Tweet(props){
         <div className="tweet">
            <h3>{props.name}</h3>
            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore debitis aliquid, numquam nulla impedit, ducimus iste eveniet quibusdam doloribus est alias deleniti quae reprehenderit dolor architecto et ad deserunt sequi?30</p> 
-            <h3>{props.likes}</h3>
+            <h3>Likes: {props.likes}</h3>
         </div>
     )
 }
